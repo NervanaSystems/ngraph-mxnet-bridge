dc0c631e1e230c899769266101ede110874c2d54
Ashok Emani <ashok.emani@intel.com>
2019-02-15 15:58:30 -0800

    update to ngraph master

--- cmake/ngraph.cmake
+++ cmake/ngraph.cmake
@@ -67,7 +67,7 @@ endif()
 ExternalProject_Add(
 	ext_ngraph
 	GIT_REPOSITORY https://github.com/NervanaSystems/ngraph.git
-	GIT_TAG ashoke/channelwise_quantize
+	GIT_TAG a5dd9f6f2158feb4be31b
 	PREFIX ngraph
 	UPDATE_COMMAND ""
 	CMAKE_ARGS ${NGRAPH_EXTRA_CMAKE_FLAGS}
